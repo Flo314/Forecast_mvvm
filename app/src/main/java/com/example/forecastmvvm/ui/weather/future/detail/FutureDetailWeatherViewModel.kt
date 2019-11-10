14281ceb4e787664fe9a45a1d9b6f62eae74f9ef
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-11-10 15:49:13 +0100

    Mise en place du style de la toolbar, fondation app (package, fragment etc...) et mise en place de la navigation

---
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.forecastmvvm.ui.weather.future
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
